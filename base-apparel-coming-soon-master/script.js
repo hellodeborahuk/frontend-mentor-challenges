769f5dd75c849c119178045c699344bde8b9dd97
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-09-22 19:48:22 +0100

    JS checks whether email is valid or ""

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,15 +1,27 @@
 const formBtn = document.getElementById("form-btn");
 const input = document.getElementById("input");
 const label = document.getElementById("label");
-const errorMessage = document.getElementById("error-message");
+const message = document.getElementById("message");
+const email = document.querySelector("input[type=email]");
+const re =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 function handleClick(e) {
   e.preventDefault();
-  formBtn.classList.remove("active");
-  formBtn.classList.add("error");
-  input.classList.remove("input-active");
-  input.classList.add("input-error");
-  label.classList.add("label-error");
-  errorMessage.style.display = "block";
+    if (re.test(email.value)) {
+    message.style.display = "block";
+    message.style.color = "var(--desaturated-red)";
+      message.textContent = "You're subscribed"
+    } else {
+      formBtn.classList.remove("active");
+      formBtn.classList.add("error");
+      input.classList.remove("input-active");
+      input.classList.add("input-error");
+      label.classList.add("label-error");
+      message.style.display = "block";
+      message.textContent = "Please provide a valid email"
+    }
 }
 
 formBtn.addEventListener("click", handleClick);
