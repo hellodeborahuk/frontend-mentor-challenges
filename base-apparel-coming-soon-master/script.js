c403afb1536df520b7a464c9bf08d627cebb768e
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-09-22 16:47:17 +0100

    Added some styling to error message

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,14 +1,18 @@
-
-const formBtn = document.getElementById("form-btn")
-const input = document.getElementById("input")
-
+const formBtn = document.getElementById("form-btn");
+const input = document.getElementById("input");
+const label = document.getElementById("label");
+const errorMessage = document.getElementById("error-message");
 function handleClick(e) {
-    e.preventDefault()
-    formBtn.classList.remove("active")
-    formBtn.classList.add("error")
-    input.classList.remove("input-active")
-    input.classList.add("input-error")
+  e.preventDefault();
+  formBtn.classList.remove("active");
+  formBtn.classList.add("error");
+  input.classList.remove("input-active");
+  input.classList.add("input-error");
+  label.classList.add("label-error");
+  errorMessage.style.display = "block";
 }
 
+formBtn.addEventListener("click", handleClick);
 
-formBtn.addEventListener("click", handleClick)
+// change class to change the design of the field/button
+// if the input isn't a valid email address or is empty then change classes
