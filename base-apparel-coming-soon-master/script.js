c51bf1268c538d56108550529902274d8d1223dc
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2021-09-24 11:28:56 +0100

    message color issue resolved

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,27 +1,35 @@
 const formBtn = document.getElementById("form-btn");
 const input = document.getElementById("input");
 const label = document.getElementById("label");
+const labelError = document.getElementsByClassName(".label-error");
 const message = document.getElementById("message");
 const email = document.querySelector("input[type=email]");
 const re =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-
 function handleClick(e) {
   e.preventDefault();
-    if (re.test(email.value)) {
+  if (re.test(email.value)) {
+    input.classList.add("input-active");
+    input.classList.remove("input-error");
+    label.classList.remove("label-error");
+    formBtn.classList.add("active");
+    formBtn.classList.remove("error");
+
     message.style.display = "block";
     message.style.color = "var(--desaturated-red)";
-      message.textContent = "You're subscribed"
-    } else {
-      formBtn.classList.remove("active");
-      formBtn.classList.add("error");
-      input.classList.remove("input-active");
-      input.classList.add("input-error");
-      label.classList.add("label-error");
-      message.style.display = "block";
-      message.textContent = "Please provide a valid email"
-    }
+    message.textContent = "You're subscribed";
+    labelError.classList.remove("input-error");
+  } else {
+    formBtn.classList.remove("active");
+    formBtn.classList.add("error");
+    input.classList.remove("input-active");
+    input.classList.add("input-error");
+    label.classList.add("label-error");
+    message.style.display = "block";
+    message.textContent = "Please provide a valid email";
+    message.style.color = "var(--soft-red)";
+  }
 }
 
 formBtn.addEventListener("click", handleClick);
